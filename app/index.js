ccf3fa5fd36a926d4e5adabae589b3964846ad42
kieranbs96 <kieranbs96@gmail.com>
2018-03-29 21:45:40 +0100

    Initial Commit

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import 'scss/style.scss';
+import 'js/lib/fontawesome-all.min.js';
+import 'js/lib/modernizr.js';
+import 'js/lib/wasap.min.js';
+
+import 'img/k-logo.png';
+import 'img/logo-green-1x.png';
+import 'img/trillo-logo.png';
+import 'img/small-knitwell.png';
+import 'img/nexter.png';
+import 'img/cigars-of-cuba-logo.png';
+import 'img/favicon.png';
